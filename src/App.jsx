f5ae89f5adb38cd0e05d242c83123da5dc3548a9
mohammedibrahimshawon <mohammedibrahimshawonaiub@gmail.com>
2024-10-18 00:44:05 +0600

    revising jsx fuctions

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,52 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todo from './Todo'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
       
       <h1>count "i love you"</h1>
-      <div className="card">
+      <Todo ttask="learn"></Todo>
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <br /><br /><br />
+        <person>i am a person</person>
+        <Student>
+        </Student>
+        
      
-      </div>
+      </div> */}
       
     </>
   )
 }
 
+function Person(){
+  
+  return <h3>i am ghoost</h3>
+}
+
+function Student(){
+  return  (
+    <div className='Student'>
+      <h1>this is a student </h1>
+      <p>name:</p>
+      <p>roll:</p>
+    </div>
+  )
+}
+
+function Developer(){
+  return(
+    <div>
+
+    </div>
+  )
+}
 export default App
